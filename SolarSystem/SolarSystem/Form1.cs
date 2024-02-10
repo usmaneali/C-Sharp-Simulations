221b204e1d4768914b94593c9330053d49ec2854
Usman Ali <84977754+usmaneali@users.noreply.github.com>
2024-02-10 11:56:20 +0500

    Add files via upload

---
+++ SolarSystem/SolarSystem/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolarSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float xe = 1, ye = 0, vxe = 0, vye = 2 * 3.142f;
+            float xj =5.2f, yj = 0, vxj = 0, vyj = 2.8f;
+            Graphics gg = this.CreateGraphics();
+            SolidBrush sy = new SolidBrush(Color.Yellow);
+            SolidBrush sw = new SolidBrush(Color.White);
+            SolidBrush sr = new SolidBrush(Color.Red);
+            SolidBrush sb = new SolidBrush(Color.Blue);
+            Planet EJ = new Planet(xe, ye, vxe, vye,xj,yj,vxj,vyj);
+            float xs = ClientSize.Width / 2, ys = ClientSize.Height / 2;
+            //Make Sun
+            gg.FillEllipse(sy, xs, ys, 20, 20);
+            //Make Earth and jupitar
+            if (radioButton2.Checked == true)
+            {
+                while (EJ.t < 10000)
+                {
+                    gg.FillEllipse(sb, xs + EJ.xe * 200, ys - EJ.ye * 200, 10, 10);
+                    gg.FillEllipse(sr, xs + EJ.xj * 50, ys - EJ.yj * 50, 10, 10);
+                    System.Threading.Thread.Sleep(2);
+                    gg.FillEllipse(sw, xs + EJ.xj * 50, ys - EJ.yj * 50, 10, 10);
+                    gg.FillEllipse(sw, xs + EJ.xe * 200, ys - EJ.ye * 200, 10, 10);
+                    EJ.revolve();
+                }
+            }
+            //Make earth
+            if (radioButton1.Checked == true)
+            {
+                while (EJ.t < 10000)
+                {
+                    gg.FillEllipse(sb, xs + EJ.xe * 200, ys - EJ.ye * 200, 10, 10);
+                    System.Threading.Thread.Sleep(10);
+                    gg.FillEllipse(sw, xs + EJ.xe * 200, ys - EJ.ye * 200, 10, 10);
+                    EJ.Revolve();
+                }
+            }
+        }
+    }
+}
---
+++ SolarSystem/SolarSystem/Planet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    class Planet
+    {
+        //Data variables
+        public float xe, ye, vxe, vye,xj,yj,vxj,vyj, re,rj,rej, delt,mj,me,ms, t,beta;
+        public Planet(float xe, float ye, float vxe, float vye, float xj, float yj, float vxj, float vyj)
+        {
+            this.xe = xe; this.ye = ye; this.vxe = vxe; this.vye = vye;
+            this.xj = xj; this.yj = yj; this.vxj = vxj; this.vyj = vyj;
+            delt = 0.004f;
+            t = 0;
+            beta = 2;
+             mj =(float)( 1.9 * Math.Pow(10, 27));
+             me = (float)(6.0 * Math.Pow(10, 24));
+             ms = (float)(2.0 * Math.Pow(10, 30));
+        }
+        public void revolve()
+        {
+
+            re = (float)Math.Sqrt(xe * xe + ye * ye);
+            rj = (float)Math.Sqrt(xj * xj+ yj * yj);
+            rej = (float)Math.Sqrt((xe-xj) * (xe -xj)+ (ye-yj) *( ye-yj));
+            vxe = vxe - 4 *(float) (Math.PI * Math.PI * xe / Math.Pow(re, beta+1))*delt-4*(float)(Math.PI*Math.PI*(mj/ms)*(xe-xj)/Math.Pow(rej,beta+1))*delt;
+            vye = vye - 4 * (float)(Math.PI * Math.PI * ye / Math.Pow(re, beta + 1)) * delt-4 *(float)(Math.PI * Math.PI * (mj / ms)*(ye - yj) /Math.Pow(rej, beta + 1)) * delt;
+            vxj = vxj - 4 * (float)(Math.PI * Math.PI * xj / Math.Pow(rj, beta + 1)) * delt - 4 * (float)(Math.PI * Math.PI*(me / ms)*(xj - xe)/ Math.Pow(rej, beta + 1)) * delt;
+            vyj = vyj - 4 * (float)(Math.PI * Math.PI * yj / Math.Pow(rj, beta + 1)) * delt - 4 * (float)(Math.PI * Math.PI*(me/ ms)*(yj - ye) /Math.Pow(rej, beta + 1)) * delt;
+            xe = xe + vxe * delt;
+            ye = ye + vye * delt;
+            xj = xj + vxj * delt;
+            yj = yj + vyj* delt;
+            t = t + delt;
+        }
+        public void Revolve()
+        {
+            re = (float)Math.Sqrt(xe * xe + ye * ye);
+            vxe= vxe- 4 * (float)(Math.PI * Math.PI * xe/ Math.Pow(re,beta + 1)) * delt;
+            vye= vye- 4 * (float)(Math.PI * Math.PI * ye/ Math.Pow(re,beta + 1)) * delt;
+            xe= xe+ vxe* delt;
+            ye = ye + vye * delt; 
+            t = t + delt;
+        }
+    }
+}

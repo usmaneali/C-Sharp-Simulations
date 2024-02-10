6f32d22e70223fabcbb7015e2ae1463b928aba9c
Usman Ali <84977754+usmaneali@users.noreply.github.com>
2024-02-10 11:50:54 +0500

    Add files via upload

---
+++ RK4_SimplePendulum/RK4_SimplePendulum/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace RK4_SimplePendulum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rk4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simplePendulumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rk4ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(985, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rk4ToolStripMenuItem
+            // 
+            this.rk4ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.simplePendulumToolStripMenuItem});
+            this.rk4ToolStripMenuItem.Name = "rk4ToolStripMenuItem";
+            this.rk4ToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.rk4ToolStripMenuItem.Text = "Rk4";
+            // 
+            // simplePendulumToolStripMenuItem
+            // 
+            this.simplePendulumToolStripMenuItem.Name = "simplePendulumToolStripMenuItem";
+            this.simplePendulumToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.simplePendulumToolStripMenuItem.Text = "SimplePendulum";
+            this.simplePendulumToolStripMenuItem.Click += new System.EventHandler(this.simplePendulumToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 547);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rk4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simplePendulumToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RK4_SimplePendulum/RK4_SimplePendulum/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RK4_SimplePendulum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Rk4
+        {
+            
+           public double f(double t, double th, double w)
+            {
+                return w;
+            }
+           public double f1(double t, double th, double w)
+        {
+          double g=9.8,l=10;
+            return (-g/l)*th;
+        }
+        }
+        private void simplePendulumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double t=2, th=5, w=1, dt = 0.05f,g=9.8f;
+            double k, k1, k2, k3, k4;
+            double l, l1, l2, l3, l4;
+        
+            Rk4 Robj = new Rk4();
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Crimson);
+            SolidBrush sb1 = new SolidBrush(Color.Cyan);
+            SolidBrush sb2 = new SolidBrush(Color.DarkBlue);
+            for (int i = 0; i < 500; i++)
+            {
+                k1 = Robj.f(t, th, w);
+              l1 = Robj.f1(t, th, w);
+                k2= Robj.f(t+dt/2.0, th+k1/2.0, w+l1/2.0);
+                    l2= Robj.f1(t+dt/2.0, th+k1/2.0, w+l1/2.0);
+                    k3= Robj.f(t+dt/2.0, th+k2/2.0, w+l2/2.0);
+                    l3= Robj.f1(t+dt/2.0, th+l2/2.0, w+l2/2.0);
+                    k4= Robj.f(t+dt, th+k3, w+l3);
+                    l4 = Robj.f1(t+dt, th+l3, w+l3);
+                    k = (k1 + 2*(k2 + k3) + k4)*(1.0/6)*dt;
+                    l = (l1 + 2 * (l2 + l3) + l4)*(1.0 / 6) * dt;
+                    th = th + k;
+                    w = w + l;
+                    t = t + dt;
+                    gg.FillEllipse(sb, 200 + (float)t*20, 250 - (float)th*30, 5, 5);
+                    gg.FillEllipse(sb1, 250 + (float)t*20, 300 - (float)w*30, 5, 5);
+                    gg.FillEllipse(sb2, 300 + (float)th * 20, 200 - (float)w * 30, 5, 5);
+
+            }
+        
+           
+        }
+    }
+}
